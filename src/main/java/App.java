af0a25980ad4001e1fa345e0afe597750801c8ff
mhlee <jigul.sw@gmail.com>
2019-01-24 14:46:48 +0900

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello be happy.");
+        System.out.println("you can do it because you are mh");
+    }
+}
